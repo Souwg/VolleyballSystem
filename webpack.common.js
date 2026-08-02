a4a1116b2028483aa4139935f8930300ae1a508b
Souwg <sansanvzla@gmail.com>
2026-08-02 23:54:50 +0000

    Enable edge-to-edge PWA layout

--- webpack.common.js
+++ webpack.common.js
@@ -46,7 +46,6 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      favicon: "4geeks.ico",
       template: "template.html",
     }),
     new Dotenv({ safe: true, systemvars: true }),
